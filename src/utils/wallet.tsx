58027ba9fafb2c73b9e34b7f4747cee23037149d
Simranjeet Singh <smrnjeet222@gmail.com>
2021-10-01 16:49:10 +0530

    Revert "Added solana wallet adapter"

--- src/utils/wallet.tsx
+++ src/utils/wallet.tsx
@@ -7,7 +7,7 @@ import React, {
 } from 'react';
 import Wallet from '@project-serum/sol-wallet-adapter';
 import { notify } from './notifications';
-// import { useConnectionConfig } from './connection';
+import { useConnectionConfig } from './connection';
 import { useLocalStorageState } from './utils';
 import { WalletContextValues } from './types';
 import { Button, Modal } from 'antd';
@@ -19,32 +19,6 @@ import {
   MathWalletAdapter,
   SolflareExtensionWalletAdapter,
 } from '../wallet-adapters';
-
-import {
-  getPhantomWallet,
-  getSolletWallet,
-  getBitpieWallet,
-  getBloctoWallet,
-  getCoin98Wallet,
-  getLedgerWallet,
-  getMathWallet,
-  getSafePalWallet,
-  getSlopeWallet,
-  getSolflareWallet,
-  getSolflareWebWallet,
-  getSolletExtensionWallet,
-  getSolongWallet,
-  // getTorusWallet,
-} from '@solana/wallet-adapter-wallets';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import {
-  ConnectionProvider,
-  WalletProvider as NewWalletProvider,
-  useWallet as newUseWallet,
-} from '@solana/wallet-adapter-react';
-import {
-  WalletModalProvider,
-} from '@solana/wallet-adapter-ant-design';
 
 const ASSET_URL =
   'https://cdn.jsdelivr.net/gh/solana-labs/oyster@main/assets/wallets';
@@ -91,89 +65,163 @@ export const WALLET_PROVIDERS = [
   },
 ];
 
-const ENDPOINT_URL =
-  'https://dawn-red-log.solana-mainnet.quiknode.pro/ff88020a7deb8e7d855ad7c5125f489ef1e9db71/';
-
 const WalletContext = React.createContext<null | WalletContextValues>(null);
 
-export function WalletProvider(props) {
-  const wallets = useMemo(
-    () => [
-      getPhantomWallet(),
-      getSolletWallet({ network: ENDPOINT_URL as WalletAdapterNetwork }),
-      getCoin98Wallet(),
-      getSolletExtensionWallet({
-        network: ENDPOINT_URL as WalletAdapterNetwork,
-      }),
-      getLedgerWallet(),
-      getBloctoWallet({ network: ENDPOINT_URL as WalletAdapterNetwork }),
-      getMathWallet(),
-      getSolflareWallet(),
-      getSolflareWebWallet(),
-      getSlopeWallet(),
-      getBitpieWallet(),
-      getSafePalWallet(),
-      getSolongWallet(),
-    ],
-    [],
+export function WalletProvider({ children }) {
+  const { endpoint } = useConnectionConfig();
+
+  const [autoConnect, setAutoConnect] = useState(false);
+  const [providerUrl, setProviderUrl] = useLocalStorageState('walletProvider');
+
+  const provider = useMemo(
+    () => WALLET_PROVIDERS.find(({ url }) => url === providerUrl),
+    [providerUrl],
   );
 
-  return (
-    <>
-      <ConnectionProvider endpoint={ENDPOINT_URL}>
-        <NewWalletProvider
-          wallets={wallets}
-          autoConnect={true}
-          onError={(error) => console.error(error)}
-          localStorageKey="walletName"
-        >
-          <WalletModalProvider>
-            <WalletInner {...props}>{props.children}</WalletInner>
-          </WalletModalProvider>
-        </NewWalletProvider>
-      </ConnectionProvider>
-    </>
-  );
-}
-
-function WalletInner(props) {
-  const newWallet = newUseWallet();
+  let [wallet, setWallet] = useState<WalletAdapter | undefined>(undefined);
 
   useEffect(() => {
-    if (newWallet.publicKey) {
-      const walletPublicKey = newWallet.publicKey.toBase58();
-      const keyToDisplay =
-        walletPublicKey.length > 20
-          ? `${walletPublicKey.substring(
-            0,
-            7,
-          )}.....${walletPublicKey.substring(
-            walletPublicKey.length - 7,
-            walletPublicKey.length,
-          )}`
-          : walletPublicKey;
+    if (provider) {
+      const updateWallet = () => {
+        // hack to also update wallet synchronously in case it disconnects
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        wallet = new (provider.adapter || Wallet)(
+          providerUrl,
+          endpoint,
+        ) as WalletAdapter;
+        setWallet(wallet);
+      };
 
-      notify({
-        message: 'Wallet update',
-        description: 'Connected to wallet ' + keyToDisplay,
+      if (document.readyState !== 'complete') {
+        // wait to ensure that browser extensions are loaded
+        const listener = () => {
+          updateWallet();
+          window.removeEventListener('load', listener);
+        };
+        window.addEventListener('load', listener);
+        return () => window.removeEventListener('load', listener);
+      } else {
+        updateWallet();
+      }
+    }
+  }, [provider, providerUrl, endpoint]);
+
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    if (wallet) {
+      wallet.on('connect', () => {
+        if (wallet?.publicKey) {
+          console.log('connected');
+          localStorage.removeItem('feeDiscountKey');
+          setConnected(true);
+          const walletPublicKey = wallet.publicKey.toBase58();
+          const keyToDisplay =
+            walletPublicKey.length > 20
+              ? `${walletPublicKey.substring(
+                  0,
+                  7,
+                )}.....${walletPublicKey.substring(
+                  walletPublicKey.length - 7,
+                  walletPublicKey.length,
+                )}`
+              : walletPublicKey;
+
+          notify({
+            message: 'Wallet update',
+            description: 'Connected to wallet ' + keyToDisplay,
+          });
+        }
+      });
+
+      wallet.on('disconnect', () => {
+        setConnected(false);
+        notify({
+          message: 'Wallet update',
+          description: 'Disconnected from wallet',
+        });
+        localStorage.removeItem('feeDiscountKey');
       });
     }
-    else {
-      notify({
-        message: 'Wallet update',
-        description: 'Disconnected from wallet',
-      });
+
+    return () => {
+      setConnected(false);
+      if (wallet && wallet.connected) {
+        wallet.disconnect();
+        setConnected(false);
+      }
+    };
+  }, [wallet]);
+
+  useEffect(() => {
+    if (wallet && autoConnect) {
+      wallet.connect();
+      setAutoConnect(false);
     }
-  }, [newWallet.connected]);
+
+    return () => {};
+  }, [wallet, autoConnect]);
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const select = useCallback(() => setIsModalVisible(true), []);
+  const close = useCallback(() => setIsModalVisible(false), []);
 
   return (
     <WalletContext.Provider
       value={{
-        wallet: newWallet as any,
-        connected: (newWallet.connected),
+        wallet,
+        connected,
+        select,
+        providerUrl,
+        setProviderUrl,
+        providerName:
+          WALLET_PROVIDERS.find(({ url }) => url === providerUrl)?.name ??
+          providerUrl,
       }}
     >
-      {props.children}
+      {children}
+      <Modal
+        title="Select Wallet"
+        okText="Connect"
+        visible={isModalVisible}
+        okButtonProps={{ style: { display: 'none' } }}
+        onCancel={close}
+        width={400}
+      >
+        {WALLET_PROVIDERS.map((provider) => {
+          const onClick = function () {
+            setProviderUrl(provider.url);
+            setAutoConnect(true);
+            close();
+          };
+
+          return (
+            <Button
+              size="large"
+              type={providerUrl === provider.url ? 'primary' : 'ghost'}
+              onClick={onClick}
+              icon={
+                <img
+                  alt={`${provider.name}`}
+                  width={20}
+                  height={20}
+                  src={provider.icon}
+                  style={{ marginRight: 8 }}
+                />
+              }
+              style={{
+                display: 'block',
+                width: '100%',
+                textAlign: 'left',
+                marginBottom: 8,
+              }}
+            >
+              {provider.name}
+            </Button>
+          );
+        })}
+      </Modal>
     </WalletContext.Provider>
   );
 }
@@ -183,9 +231,20 @@ export function useWallet() {
   if (!context) {
     throw new Error('Missing wallet context');
   }
+
   const wallet = context.wallet;
   return {
     connected: context.connected,
     wallet: wallet,
+    providerUrl: context.providerUrl,
+    setProvider: context.setProviderUrl,
+    providerName: context.providerName,
+    select: context.select,
+    connect() {
+      wallet ? wallet.connect() : context.select();
+    },
+    disconnect() {
+      wallet?.disconnect();
+    },
   };
 }
